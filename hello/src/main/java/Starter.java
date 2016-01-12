3988ad14d9d670658520f9bedbe36ec7925ef1a2
Liang Ding <dl88250@gmail.com>
2016-01-12 16:54:28 +0800

    formatting

--- hello/src/main/java/Starter.java
+++ hello/src/main/java/Starter.java
@@ -16,13 +16,10 @@
 
 import org.b3log.latke.Latkes;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.util.log.Log;
-import org.eclipse.jetty.util.log.Slf4jLog;
 import org.eclipse.jetty.webapp.WebAppContext;
 
-
 /**
- * Solo with embedded Jetty, <a href="https://github.com/b3log/solo/issues/12037">standalone mode</a>.
+ * Starter with embedded Jetty, <a href="https://github.com/b3log/solo/issues/12037">standalone mode</a>.
  *
  * <ul>
  * <li>Windows: java -cp WEB-INF/lib/*;WEB-INF/classes org.b3log.solo.Starter</li>
@@ -30,28 +27,28 @@ import org.eclipse.jetty.webapp.WebAppContext;
  * </ul>
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.1.0.7, Dec 23, 2015
- * @since 1.2.0
+ * @version 1.0.0.0, Jan 12, 2016
  */
 public class Starter {
 
-
-    public static void main(String [] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         Latkes.setScanPath("org.b3log.latke.demo.hello"); // For Latke IoC
         Latkes.initRuntimeEnv();
+
         String path = Thread.currentThread().getContextClassLoader().getResource("").getFile();
         path = path.replace("target/classes/", "");
         path = path.replace("classes/", "");
         String web = path + "src/main/webapp/";
+
         final Server server = new Server(8080);
         final WebAppContext root = new WebAppContext();
         root.setParentLoaderPriority(true);
         root.setContextPath("/");
         root.setResourceBase(web);
         root.setDescriptor(web + "/WEB-INF/web.xml");
+
         server.setHandler(root);
         server.start();
         server.join();
     }
 }
-
