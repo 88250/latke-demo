12a670f7987148eacea86dd0d6f066fd5be90591
Liang Ding <DL88250@gmail.com>
2016-09-18 23:15:41 +0800

    改进 - 内嵌 jetty 启动

--- hello/src/main/java/org/b3log/latke/demo/hello/Starter.java
+++ hello/src/main/java/org/b3log/latke/demo/hello/Starter.java
@@ -1,21 +1,9 @@
 package org.b3log.latke.demo.hello;
 
-/*
- * Copyright (c) 2010-2016, b3log.org & hacpai.com
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import java.io.File;
 import org.b3log.latke.Latkes;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 /**
  * Starter with embedded Jetty, <a href="https://github.com/b3log/solo/issues/12037">standalone mode</a>.
@@ -26,11 +14,34 @@ import org.b3log.latke.Latkes;
  * </ul>
  *
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.0.1.1, Sep 4, 2016
+ * @version 1.0.1.0, Jul 6, 2016
  */
 public class Starter {
 
     public static void main(String[] args) throws Exception {
-        Latkes.boot(Starter.class, args);
+        Latkes.setScanPath("org.b3log.latke.demo.hello"); // For Latke IoC
+        Latkes.initRuntimeEnv();
+
+        String webappDirLocation = "src/main/webapp/"; // POM structure in dev env
+        final File file = new File(webappDirLocation);
+        if (!file.exists()) {
+            webappDirLocation = "."; // production environment
+        }
+
+        final Server server = new Server(Integer.valueOf(Latkes.getServerPort()));
+        final WebAppContext root = new WebAppContext();
+        root.setParentLoaderPriority(true); // Use parent class loader
+        root.setContextPath("/");
+        root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
+        root.setResourceBase(webappDirLocation);
+        server.setHandler(root);
+
+        try {
+            server.start();
+        } catch (final Exception e) {
+            e.printStackTrace();
+
+            System.exit(-1);
+        }
     }
 }
